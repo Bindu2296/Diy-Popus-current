73daac1ff08c0e093670b52e06144d500891c9ac
Bindu2296 <48077296+Bindu2296@users.noreply.github.com>
2019-04-12 09:51:45 +0530

    first commit

---
+++ UI/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './_guards/auth.guard';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+
+
+const routes: Routes = [
+  {path: '', component: LandingpageComponent},
+  {path: 'landingpage', component: LandingpageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'user-dashboard', component: UserDashboardComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
